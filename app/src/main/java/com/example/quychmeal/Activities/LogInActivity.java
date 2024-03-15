62db165e51b368557a5f16073d54500d9725fd4a
tai096 <trinhtaii09@gmail.com>
2024-03-15 17:07:30 +0700

    add LogInActivity

---
+++ app/src/main/java/com/example/quychmeal/Activities/LogInActivity.java
@@ -0,0 +1,16 @@
+package com.example.quychmeal.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quychmeal.R;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
