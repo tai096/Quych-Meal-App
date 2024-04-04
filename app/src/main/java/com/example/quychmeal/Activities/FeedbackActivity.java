0aaad63e25699990cf2a2cc50b2e451bba88b006
tai096 <trinhtaii09@gmail.com>
2024-04-04 08:07:13 +0700

    add feedback screen

--- app/src/main/java/com/example/quychmeal/Activities/FeedbackActivity.java
+++ app/src/main/java/com/example/quychmeal/Activities/FeedbackActivity.java
@@ -1,6 +1,8 @@
 package com.example.quychmeal.Activities;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,17 +12,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.quychmeal.R;
 
-public class FeedbackActivity extends AppCompatActivity {
+public class FeedbackActivity extends RootActivity {
+    private ImageButton btnFbGoBack;
+    private EditText editTxtFeedback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_feedback);
+
+        btnFbGoBack = findViewById(R.id.btnFbGoBack);
+        editTxtFeedback = findViewById(R.id.editTxtFeedback);
+
+        handleGoBack();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+    private void handleGoBack (){
+        btnFbGoBack.setOnClickListener(v -> {
+            FeedbackActivity.this.finish();
         });
     }
 }
