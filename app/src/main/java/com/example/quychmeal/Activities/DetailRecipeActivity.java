6f3e5df9354c56ea58d17eb533a6be387c3689eb
NguyenHongQuan1208 <nquan1208@gmail.com>
2024-04-09 07:39:52 +0700

    Doing detail recipe xml

--- app/src/main/java/com/example/quychmeal/Activities/DetailRecipeActivity.java
+++ app/src/main/java/com/example/quychmeal/Activities/DetailRecipeActivity.java
@@ -1,6 +1,15 @@
 package com.example.quychmeal.Activities;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.quychmeal.R;
+import com.example.quychmeal.databinding.ActivityMainBinding;
 
 public class DetailRecipeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_recipe);
+    }
 }
