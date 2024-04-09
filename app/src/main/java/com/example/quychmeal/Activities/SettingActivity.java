da57784ce39ae42f60cc444b6b205aa1f25baaa5
tai096 <trinhtaii09@gmail.com>
2024-04-09 17:31:21 +0700

    feat: change languages

--- app/src/main/java/com/example/quychmeal/Activities/SettingActivity.java
+++ app/src/main/java/com/example/quychmeal/Activities/SettingActivity.java
@@ -1,10 +1,17 @@
 package com.example.quychmeal.Activities;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
@@ -17,12 +24,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.quychmeal.R;
 
+import java.util.Locale;
+
 public class SettingActivity extends RootActivity {
     private ImageButton btnSettingGoBack;
     private Switch switchTheme;
     private boolean darkMode;
-    SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
+    private Spinner langSpinner;
+    public static final String[] languages = {"Select Language", "English", "Vietnamese", "Japanese"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +42,18 @@ public class SettingActivity extends RootActivity {
 
         setContentView(R.layout.activity_setting);
 
+
         btnSettingGoBack = findViewById(R.id.btnSettingGoBack);
         switchTheme = findViewById(R.id.swichTheme);
 
-        sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE);
+        langSpinner = findViewById(R.id.languageSpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, languages);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        langSpinner.setAdapter(adapter);
+        langSpinner.setSelection(0);
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE);
         darkMode = sharedPreferences.getBoolean("darkMode", false);
 
         if (darkMode) {
@@ -57,6 +75,7 @@ public class SettingActivity extends RootActivity {
         });
 
         handleGoBack();
+        handleSelectLanguage();
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -68,6 +87,32 @@ public class SettingActivity extends RootActivity {
     private void handleGoBack() {
         btnSettingGoBack.setOnClickListener(v -> {
             SettingActivity.this.finish();
+        });
+    }
+
+    private void handleSelectLanguage() {
+        langSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedLang = adapterView.getItemAtPosition(i).toString();
+                switch (selectedLang) {
+                    case "English":
+                        setLocale("en");
+                        startActivity(new Intent(SettingActivity.this, MainActivity.class));
+                        break;
+                    case "Vietnamese":
+                        setLocale("vi");
+                        startActivity(new Intent(SettingActivity.this, MainActivity.class));
+                        break;
+                    case "Japanese":
+                        setLocale("ja");
+                        startActivity(new Intent(SettingActivity.this, MainActivity.class));
+                        break;
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
         });
     }
 
