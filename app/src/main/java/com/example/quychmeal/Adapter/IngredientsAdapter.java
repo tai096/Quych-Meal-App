7c1eb484375d13e5c6c9ec6f115e36c56b2b2277
tai096 <trinhtaii09@gmail.com>
2024-04-12 22:29:19 +0700

    hotfix:selecting ingredients

--- app/src/main/java/com/example/quychmeal/Adapter/IngredientsAdapter.java
+++ app/src/main/java/com/example/quychmeal/Adapter/IngredientsAdapter.java
@@ -14,11 +14,13 @@ import java.util.ArrayList;
 
 public class IngredientsAdapter extends BaseAdapter {
     private ArrayList<Ingredient> ingredientArrayList;
+    private ArrayList<Integer> selectedIngredients;
     private Context context;
     LayoutInflater layoutInflater;
 
-    public IngredientsAdapter(ArrayList<Ingredient> ingredientArrayList, Context context) {
+    public IngredientsAdapter(ArrayList<Ingredient> ingredientArrayList, ArrayList<Integer> selectedIngredients, Context context) {
         this.ingredientArrayList = ingredientArrayList;
+        this.selectedIngredients = selectedIngredients;
         this.context = context;
     }
 
@@ -29,23 +31,21 @@ public class IngredientsAdapter extends BaseAdapter {
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return ingredientArrayList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         if (layoutInflater == null) {
             layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
         }
         if (convertView == null) {
             convertView = layoutInflater.inflate(R.layout.ingredient_item, null);
-
         }
 
         // Get the current ingredient
@@ -57,13 +57,27 @@ public class IngredientsAdapter extends BaseAdapter {
         TextView nameIngredient = convertView.findViewById(R.id.nameIngredient);
         nameIngredient.setText(ingredientArrayList.get(position).getName());
 
+        // Check if the ingredient ID is in selectedIngredients
+        if (selectedIngredients.contains(ingredient.getId())) {
+            // Change the background color of the TextView
+            nameIngredient.setBackgroundResource(R.drawable.custom_selected_item);
+            nameIngredient.setTextColor(context.getResources().getColor(R.color.white));
+        } else {
+            // Change the background color back to default
+            nameIngredient.setBackgroundResource(R.drawable.custom_button);
+            nameIngredient.setTextColor(context.getResources().getColor(R.color.darkMode));
+        }
 
         return convertView;
+    }
+
+    public void setSelectedIngredients(ArrayList<Integer> selectedIngredients) {
+        this.selectedIngredients = selectedIngredients;
+        notifyDataSetChanged();
     }
 
     public void filterList(ArrayList<Ingredient> filteredList) {
         ingredientArrayList = filteredList;
         notifyDataSetChanged();
     }
-
 }
